cff5bfd49588a4ae88b914d264d52f84d69d4d71
tGuenot <tom.guenot@free.fr>
2025-05-16 14:51:30 +0200

    #9 rajout d'une option sur le main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import multiprocessing
 import time
 import numpy as np
 from GPS.Button import Button
+from GPS import get_position
+from IMU import get_imu_data
 
 from filtres import *
 from mesures import Mesures
@@ -9,52 +11,50 @@ from mesures import Mesures
 from datetime import datetime as dtime
 
 def F(dt:float, n:int=6):
+    """Definition dynamique de la matrice F"""
     F_dt = np.eye(n)
     F_dt [0:n//2, n//2:]  = dt*np.eye(n//2)
     return F_dt
 
 def G(dt:float, n:int=3):
+    """Definition dynamique de la matrice G"""
     return np.block([[1/2*dt**2*np.eye(n)],
                      [dt*np.eye(n)]
                     ])
 
 def initialisation():
+    """Initialisation du filtre de Kalman"""
     H = np.block([np.eye(3), np.zeros((3, 3))])
     filtre = FiltreKalman(F(0), H, G(0)) # Rajouter R et Q
     filtre.x = Mesures().gnss
 
     return filtre
 
-def get_donnees_imu (file_imu,arret):
+def get_donnees_imu (file_imu:Queue, arret:Queue):
+    """Processus de récupération des données de l'IMU"""
     rpi = Mesures()
-    i = 1
     while arret.empty():
-        i += 1
-        #file_imu.put(rpi.imu)
+        file_imu.put(rpi.imu)
         file_imu.put(i)
         time.sleep(0.1)
 
-def get_donnees_gnss(file_gnss,arret):
+def get_donnees_gnss(file_gnss:Queue, arret:Queue):
+    """Processus de récupération des données GPS"""
     rpi = Mesures()
-    i = 1
     while arret.empty():
-        i += 1
-        #file_gnss.put(rpi.gnss)
-        file_gnss.put(i)
+        file_gnss.put(rpi.gnss)
         time.sleep(1)
-        
 
-
-if __name__ == "__main__":
-    filtre = initialisation()
-    
+def acquisition_directe(filtre:FiltreKalman)->None:
+    """Fonction d'acquisition en directe des données de la RPi"""
     #initialisation multiprocess
     file_imu = multiprocessing.Queue()
     file_gnss = multiprocessing.Queue()
     file_arret = multiprocessing.Queue()
     
+    #initialisation des differents processus qui tourneront en parallèle
     p_imu = multiprocessing.Process(target=get_donnees_imu, args=(file_imu,file_arret,))
-    p_gnss = multiprocessing.Process(target=get_donnees_gnss, args=(file_gnss,file_arret,))
+    p_gnss = multiprocessing.Process(target=get_imu_data, args=(file_gnss,file_arret,))
     
     p_imu.start()
     p_gnss.start()
@@ -74,7 +74,6 @@ if __name__ == "__main__":
     
     #main
     while file_arret.empty():
-
         try:            
             if not file_imu.empty():
                 u = file_imu.get()
@@ -89,11 +88,47 @@ if __name__ == "__main__":
                 print(u, y_new)
                 y_old = y_new
                 
-
             #afficher(filtre.x)
             time.sleep(0.1)
         except KeyboardInterrupt:
             b.cleanup()
             break
 
-    print("Exit...")
+def acquisition_csv(filtre:FiltreKalman)->None:
+    """Fonction d'écriture des acquisitions RPi dans des csv"""
+
+    path_donnees_gps = "output/GPS/acquisition"
+    path_donnees_imu = "output/IMU/acquisition"
+
+    #initialisation des differents processus qui tourneront en parallèle
+    p_imu = multiprocessing.Process(target=get_position, args=(path_donnees_gps,))
+    p_gnss = multiprocessing.Process(target=get_donnees_gnss, args=(path_donnees_gps,))
+    print("Acquisitions en cours...")
+
+    #initialisation bouton
+    b = Button(17)
+    def press():
+        file_arret.put("fin")
+    b.on_press(press)
+    
+    #attente de la fin des acquisitions
+    p_imu.join()
+    p_gnss.join()
+    print("Acquisitions finies !\n")
+
+    #Recuperation des donnees IMU et GPS
+    print("Traitement en cours...")
+    j = 0
+    y = donnees_gps[0]
+    for i in range(len(donnees_imu)):
+        u = donnees_imu[i]
+        if y[0] >= u[0]:
+            filtre(u[1:], y[:1])
+            j += 1
+            y = donnees_gps[j]
+        else:
+            filtre(u[:1])
+    print("Fin du traitement\n")
+
+if __name__ == "__main__":
+    pass
