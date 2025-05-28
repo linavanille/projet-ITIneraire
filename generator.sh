9db5ec2efea4139ff33cb71eb9ea8c51e4e6ca1b
tguenot <tom.guenot@insa-rouen.fr>
2025-05-28 02:35:13 +0200

    On fait ce qu'on peut

--- generator.sh
+++ generator.sh
@@ -2,9 +2,11 @@
 
 if [ "$1" = 'clean' ]; then
     echo "rm ./output/HTML/plot*/*"
-    echo "rm ./output/CSV_Filtre/*"
     rm ./output/HTML/plot*/*
-    rm ./output/CSV_Filtre/*
+    echo "rm ./output/CSV_Filtre*/*"
+    rm ./output/CSV_Filtre*/*
+    echo "rm ./output/GPX/*"
+    rm ./output/GPX/*
 
 elif [ "$1" = 'raw' ]; then
     echo "python3 src/analyse_acquisitions.py -- raw"
@@ -17,9 +19,19 @@ elif [ "$1" = '-a' ]; then
 elif [ "$1" = "-n" ]; then
     echo "clean output"
     rm ./output/HTML/plot*/*
-    rm ./output/CSV_Filtre/*
+    rm ./output/CSV_Filtre*/*
+    rm ./output/GPX/*
     echo "python3 src/analyse_acquisitions.py -- all"
     python3 src/analyse_acquisitions.py -- all
+    echo "creation des gpx"
+    ipython3 convertisseur/csv_to_gpx.py output/CSV_Filtre/magellan.csv output/GPX/filtre.gpx
+    ipython3 convertisseur/csv_to_gpx.py output/DoubleAcquisition/acquisitionGPS.csv output/GPX/raw.gpx
+
+elif [ "$1" = '-gpx' ]; then
+    echo "conversion gpx de l'output filtre"
+    ipython3 convertisseur/csv_to_gpx.py output/CSV_Filtre/magellan.csv output/GPX/filtre.gpx
+    echo "conversion gpx des données brutes"
+    ipython3 convertisseur/csv_to_gpx.py output/DoubleAcquisition/acquisitionGPS.csv output/GPX/raw.gpx
 else
     echo "python3 src/analyse_acquisitions.py -- filtre"
     python3 src/analyse_acquisitions.py -- filtre
